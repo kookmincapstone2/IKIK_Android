480db33a004de26a7c2747eb93335d529120396a
kwtjdtn <kwtjdtn1@naver.com>
2020-09-26 04:09:28 +0900

    SignUp 버튼-> Text 수정

--- Component/Main.js
+++ Component/Main.js
@@ -15,22 +15,29 @@ class Main extends Component {
   render() {
     return (
       <View style={styles.mainView}>
-        <Image source={LogoImg} style = {{ marginVertical: 10}}/>
-        <Text style = {{ marginVertical: 10}}> Welcome</Text>
+        <Image source={LogoImg} 
+        style = {{ 
+            marginTop: 150, 
+            width: 100, 
+            height: 110,
+            marginBottom:10}}/>
+        <Text 
+        style = {{
+            marginBottom: 50,
+            fontSize: 18
+            }}> Welcome to IKIK!</Text>
         <TextInput placeholder="ID" style = {styles.inputBox} underlineColorAndroid='rgba(0,0,0,0)' />
         <TextInput placeholder="Password" style = {styles.inputBox} underlineColorAndroid='rgba(0,0,0,0)'/>
         <Button
-          title="Login"
+          title="Log In"
           onPress={() => Alert.alert('test')}
           color="#87ceaf"
-          style = {{ marginVertical: 30, width: 50, height: 30}}
+          style = {styles.button}
         />
-        <Button
-          title="Sing up"
-          onPress={() => Alert.alert('test')}
-          color="#87ceaf"
-          style = {{ marginVertical: 30, width: 50, height: 30}}  
-        />
+        <View style= {styles.signupCont}>
+            <Text style = {styles.signupText}>Don't have an account yet? </Text>
+            <Text style = {styles.signupBtn}>Sing Up</Text>
+        </View>
       </View>
     );
   }
@@ -40,16 +47,41 @@ const styles = StyleSheet.create({
   mainView: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
     fontSize: 18,
   },
   inputBox: {
-      width: 200,
-      height: 30,
-      marginVertical: 10,
+      width: 300,
+      height: 50,
+      marginVertical: 7,
       backgroundColor: '#c7ffef',
       paddingHorizontal: 15,
       borderRadius: 25,
+  },
+  button: {
+      borderRadius: 25,
+      fontWeight: '500',
+      textAlign: 'center',
+      marginVertical: 30,
+      width: 300,
+      height: 50,
+      fontWeight: '700',
+  },
+  signupCont : {
+      flexGrow: 1,
+      alignItems: 'center',
+      justifyContent : 'flex-end',
+      paddingVertical: 10,
+      flexDirection: 'row'
+  },
+  signupText: {
+    //   color: 'rgba(255,255,255,0.6)',
+      fontSize: 16
+  },
+  signupBtn: {
+      color: '#87ceaf',
+      fontSize: 16,
+      fontWeight: '700', 
   }
 });
 
